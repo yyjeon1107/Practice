53da8d2a1e19dfd15866d283ec4162816064c4b8
ycy227 <mimiming7@gmail.com>
2022-04-18 15:07:11 +0900

    .
    .

--- MyMap/src/main/java/com/map/DemoApplication.java
+++ MyMap/src/main/java/com/map/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("hello merge test");
+		System.out.println("hello2");
+		
 	}
 
 }
